9b2bace42214f9fb7f97d09ba005f448ee5d2ed0
Priya Pandey <122604459+Priya040804@users.noreply.github.com>
2025-05-22 23:36:25 +0530

    Update AdminRoutes.js

--- AdminRoutes.js
+++ AdminRoutes.js
@@ -4,20 +4,13 @@ const { viewFlagged, aggregateBalances, topUsersByBalance, topUsersByVolume } = 
 const { userVerification } = require('../Middlewares/Authmiddleware');
 const { isAdmin } = require('../Middlewares/Authmiddleware');
 
-// Protect admin routes
 router.use(userVerification);
-//Check if user is admin
 router.use(isAdmin);
-// View flagged fraud transactions
 router.get('/flagged', viewFlagged);
 
-// Get all users with their aggregated balances
 router.get('/balances', aggregateBalances);
-
-// Get top users by balance (query: ?limit=n)
 router.get('/top-balance', topUsersByBalance);
 
-// Get top users by transaction volume (query: ?limit=n)
 router.get('/top-volume', topUsersByVolume);
 
 module.exports = router;
